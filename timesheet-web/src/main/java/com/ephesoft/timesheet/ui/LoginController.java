ac1cbe866127bcf4b4d1f23095c7935bca7f603f
mayank <mayank.sood@ephesoft.com>
2016-03-11 18:13:39 +0530

    added web module

--- timesheet-web/src/main/java/com/ephesoft/timesheet/ui/LoginController.java
+++ timesheet-web/src/main/java/com/ephesoft/timesheet/ui/LoginController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ephesoft.timesheet.core.model.User;
 import com.ephesoft.timesheet.services.UserService;
@@ -16,13 +18,13 @@ public class LoginController {
 	
 	@Autowired UserService userService;
 
-    @RequestMapping(value = "/u", method = { RequestMethod.GET })
-    public String getDeals(HttpServletRequest request, Model model) {
+    @RequestMapping(value = "/u/{userId}", method = { RequestMethod.GET })
+    public String getDeals(@PathVariable("userId") long userId, HttpServletRequest request, Model model) {
     	User user = userService.validateUser("", "");
     	user = new User();
-    	user.setId(23);
+    	user.setId(userId);
         model.addAttribute("user", user);
-        return "user";
+        return "/index.jsp";
 
     }
 
--- timesheet-web/src/main/webapp/index.jsp
+++ timesheet-web/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
+<%@page import="com.ephesoft.timesheet.core.model.User"%>
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>UserId
+
+<%=((User)request.getAttribute("user")).getId() %>
+</h2>
 </body>
 </html>
